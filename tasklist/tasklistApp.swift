64bc211a610db57490ec930084c0e9bdc65723c1
GuiLSDias <glsd273@gmail.com>
2025-12-12 22:45:32 -0300

    Initial Commit

---
+++ tasklist/tasklistApp.swift
@@ -0,0 +1,17 @@
+//
+//  tasklistApp.swift
+//  tasklist
+//
+//  Created by Guilherme Dias on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct tasklistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
